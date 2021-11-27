87fcd046028702166338a52bf336397618a71bfc
moses <academicsnotes@gmail.com>
2021-11-27 14:04:43 +0800

    Commit script

---
+++ OOP_Ruby/classExample_01.rb
@@ -0,0 +1,20 @@
+# Defining class
+# Note: first letter of class name has to be uppercase
+class ExampleClass
+
+    # defining a method
+    def methodExample
+
+        puts "Test Method"
+
+        # end a method
+    end
+
+    # end of a class
+end
+
+# Creating object
+obj = ExampleClass.new
+
+# Calling method using object
+obj.methodExample
---
+++ basic/TestingMethod.rb
@@ -0,0 +1,5 @@
+def testing
+    puts "Testing 123"
+end
+
+testing
