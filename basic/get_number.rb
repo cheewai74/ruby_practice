0ef9a6ef8642a46abbf8fb4d5f074eca96d23593
moses <academicsnotes@gmail.com>
2022-02-01 08:52:31 +0800

    Commit script

---
+++ basic/get_number.rb
@@ -0,0 +1,9 @@
+#  Note: Execute this program in Terminal Tab
+#  ruby get_number.rb
+#  else, it will stays in vscode running mode, not getting any response.
+
+puts "Welcome to 'Get My Number!' "
+print "What's your name? "
+# input = "Micky Mouse"
+input = gets
+puts "Welcome, #{input}"
