2e1eac59ee4afa567491c00a7bc8c9e9c3e2d345
moses <academicsnotes@gmail.com>
2022-02-02 13:16:42 +0800

    Commit script

---
+++ OOP_Ruby/overrideMethods.rb
@@ -0,0 +1,56 @@
+class Animal
+
+    attr_reader :name, :age
+
+    def name=(value)
+        if value ==""
+            raise "Name can't be blank"
+        end 
+        @name = value
+    end 
+
+    def age=(value)
+        if value < 0
+            raise "An age of #{value} isn't valid!"
+        end 
+        @age = value
+    end 
+
+    def talk
+        puts "#{@name} says Bark!"
+    end 
+
+    def move(destination)
+        puts "#{@name} runs to the #{destination}."
+    end 
+
+    def report_age
+        puts "#{@name} is #{@age} years old."
+    end 
+
+end 
+
+class Dog < Animal
+end
+
+class Bird < Animal
+    #Overrides the inherited method
+    def talk 
+        puts "#{@name} says Chirp Chirp!"
+    end 
+end 
+
+class Cat < Animal
+    #Overrides the inherited method
+    def talk 
+        puts "#{@name} says Meow!"
+    end 
+end
+
+whiskers = Cat.new
+whiskers.name = "Whiskers"
+whiskers.talk
+
+polly = Bird.new
+polly.name = "Polly"
+polly.talk
