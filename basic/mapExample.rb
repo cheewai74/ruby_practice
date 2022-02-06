98ada854ad29862425bf28d8be01d4d75eb43cd5
moses <academicsnotes@gmail.com>
2022-02-06 22:43:53 +0800

    Commit script

--- basic/mapExample.rb
+++ basic/mapExample.rb
@@ -14,3 +14,12 @@ p squares
 
 squares = [2,3,4].map{|number| number ** 2}
 p squares
+
+numbers = [5,7,8]
+cubes = []
+numbers.each do |number|
+    cubes << number ** 3
+end 
+
+cubes = [5,6,7].map{|number|number ** 3 }
+p cubes
