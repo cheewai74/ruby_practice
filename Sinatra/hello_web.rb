202c84348d1668d7d8701cbae4e648026f32cd53
moses <academicsnotes@gmail.com>
2022-02-07 22:56:23 +0800

    Commit script

--- Sinatra/hello_web.rb
+++ Sinatra/hello_web.rb
@@ -1,5 +1,25 @@
+"""
+Execute in cmder
+"""
 require 'sinatra'
 
 get '/' do
   'Hello world!'
+end
+
+# Setup a route for Get Request with a path of "/hello"
+#http://127.0.0.1:4567/hello
+get '/hello' do
+  'Hello, web!'
+end
+
+# Setup a route for Get Request with a path of "/salut"
+# http://127.0.0.1:4567/salut
+get '/salut' do
+  'Salut web!'
+end
+
+# Setup a route for Get Request with a path of "/John"
+# http://127.0.0.1:4567/John
+  'John''s, web!'
 end
