99fd532bacac14208f3f08d9ac05d09f6e3a6fa7
moses <academicsnotes@gmail.com>
2022-02-13 00:23:53 +0800

    Commit script

--- Sinatra/chapter2/main.rb
+++ Sinatra/chapter2/main.rb
@@ -1,24 +1,50 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'slim'
+require 'sass'
 
+# sass
+get('/styles.css'){sass :styles}
+
+# get '/' do
+#     erb :home
+# end
+
+# slim
 get '/' do
-    erb :home
+    slim :home
 end
 
+# get '/about' do
+#     @title = "All about this website"
+#     erb :about
+# end
+
+# slim
 get '/about' do
     @title = "All about this website"
-    erb :about
+    slim :about
 end
 
+# get '/contact' do
+#     erb :contact
+# end
+
+# slim
 get '/contact' do
-    erb :contact
+    slim :contact
 end
 
 set :public_folder, 'public'
 set :views, 'views'
 
-# Sinatra has a special method for that, called not_found. 
-# It’s used just like the route handler blocks:
+# # Sinatra has a special method for that, called not_found. 
+# # It’s used just like the route handler blocks:
+# not_found do
+#     erb :not_found
+# end
+
+# slim
 not_found do
-    erb :not_found
+    slim :not_found
 end
